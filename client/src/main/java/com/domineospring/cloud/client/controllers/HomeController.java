68495cf778f539652705b136fdbeb558d2a0e1bd
Fernando W S Furtado <feh.wilinando@gmail.com>
2018-01-04 22:42:01 -0200

    Refactoring Example

--- client/src/main/java/com/domineospring/cloud/client/controllers/HomeController.java
+++ client/src/main/java/com/domineospring/cloud/client/controllers/HomeController.java
@@ -4,15 +4,31 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class HomeController {
 
-    @Value("${my.name}")
-    private String name;
+    @Value("${spring.datasource.username}")
+    private String username;
 
-    @GetMapping("/say-my-name")
-    public String sayMyName(){
-        return name;
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @GetMapping("/datasource")
+    public String configurations(){
+
+        Map<String, String> response = new HashMap<>();
+
+        response.put("username", username);
+        response.put("password", password);
+        response.put("url", url);
+
+        return response.toString();
     }
 
 }
